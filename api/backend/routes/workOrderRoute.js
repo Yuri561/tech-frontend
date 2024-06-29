46f5588bd2bb18b422c960e53c0b919cd08e6983
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-06-29 13:28:43 -0400

    moved

--- api/backend/routes/workOrderRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getWorkOrders, createWorkOrder, deleteWorkOrder } = require('../controllers/workOrderController');
-router.get('/workorders', getWorkOrders);
-router.post('/workorders', createWorkOrder);
-router.delete('/workorders/:id', deleteWorkOrder); // Ensure the route includes ':id'
-
-module.exports = router;
